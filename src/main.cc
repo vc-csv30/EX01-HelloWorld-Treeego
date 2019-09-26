8f0e73a41eb8f14d11eb1fc43fd465d07b65b106
Santiago Barajas <santiago_barajas2@my.vcccd.edu>
2019-09-26 12:11:34 -0700

    turning in late so confused

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,21 @@
 //
 // Created by aknight on 8/2/19.
 //
+/**
+ * CS V30 Beginning C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [CHANGE THIS INFORMATION]
+ */
 
+#include <iostream>
+#include <csv30>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "hello world!";
+    return EXIT_SUCCESS;
+}
